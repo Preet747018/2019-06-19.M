d54e367d90e7fd873329853f6f0e4860df646e80
Lovepreet Kaur <747018@student.tor.loc>
2019-06-19 15:21:03 -0400

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+function CreateCircle(radius){
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    }
+}
+const circle = CreateCircle(1);
+circle.draw();
+// Constructor Function 
+
+function Circle(radius) {
+    console.log('this', this);
+    this.radius = radius;
+    this.draw = function(){
+        console.log("draw");
+    }
+}
+const another = new Circle(1);
